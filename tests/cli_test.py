99748e2561694293468624df8821cc1f3b447922
konradhorber <konrad.horber@gmail.com>
2025-03-26 15:23:26 +0100

    Add yield to rules level allowing multiple problems from different rules to be returned

---
+++ tests/cli_test.py
@@ -0,0 +1,27 @@
+from validateactions import cli
+import tempfile
+import os
+
+def test_run():
+    workflow = """
+name: test
+on: psuh
+jobs:
+  build:
+    runs-on: ubuntu-latest
+    steps:
+      - name: Notify Slack
+        uses: 8398a7/action-slack@v3
+        with:
+          unknown_input: 'test'
+"""
+    with tempfile.NamedTemporaryFile(mode='w', delete=False) as tmp:
+        tmp.write(workflow)
+        tmp_path = tmp.name 
+
+    try:
+        cli.run(tmp_path)
+    finally:
+        os.remove(tmp_path)
+    # tokens = list(parser.tokenize(workflow))
+    assert None is None
